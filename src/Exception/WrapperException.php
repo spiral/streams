227fe291de8a8ef540140e8d9ffaaa579e1f535d
valentin v / vvval <valentin@spiralscout.com>
2018-10-08 15:17:29 +0300

    Init commit

---
+++ src/Exception/WrapperException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Streams\Exception;
+
+/**
+ * StreamWrapper specific exception.
+ */
+class WrapperException extends \RuntimeException
+{
+}
